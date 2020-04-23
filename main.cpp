9071c4fb784579ce4bbfb780929ed093fe24f863
alllef <alekz29@gmail.com>
2020-04-23 19:42:41 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+#include <fstream>
+#include<Windows.h>
+
+using namespace std;
+
+struct Rib {
+public:
+    int start;
+    int end;
+    double weight;
+};
+
+void initializeGraph(int &picks, int &ribs, vector<Rib> &structRibs);
+
+void sortRibs(int &picks, int &ribs, vector<Rib> &structRibs);
+
+
+int main() {
+    vector<Rib> ribsList;
+    int n = 0, m = 0;
+    return 0;
+}
+
+void initializeGraph(int &picks, int &ribs, vector<Rib> &structRibs) {
+    Rib myRib{};
+    ifstream inFile;
+    inFile.open("myGraph.txt");
+
+    if (!inFile.is_open()) cout << "It is not open" << endl;
+    inFile >> picks >> ribs;
+
+    for (int i = 0; i < ribs; i++) {
+        inFile >> myRib.start >> myRib.end >> myRib.weight;
+
+        structRibs.push_back(myRib);
+    }
+    inFile.close();
+}
+
+void sortRibs(int &picks, int &ribs, vector<Rib> &structRibs) {
+
+    Rib tmp{};
+    for (int i = 0; i < ribs - 1; i++) {
+        for (int j = 0; j < ribs - 1; j++) {
+            if ((structRibs[j].start + structRibs[j].end) > (structRibs[j + 1].start + structRibs[j + 1].end)) {
+
+                tmp = structRibs[j];
+                structRibs[j] = structRibs[j + 1];
+                structRibs[j + 1] = tmp;
+
+            }
+        }
+    }
+}
